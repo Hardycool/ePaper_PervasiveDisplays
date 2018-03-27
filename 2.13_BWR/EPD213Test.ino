ea09c9b02159067cf720f14ec7c1002bc6c4316d
Hardy-PDi <hardy_kuo@pervasivedisplays.com>
2018-03-27 15:48:20 +0800

    Update EPD213Test.ino

--- 2.13_BWR/EPD213Test.ino
+++ 2.13_BWR/EPD213Test.ino
@@ -1,5 +1,5 @@
 /* 
- *  This example code is for PDi 1.54" BWR EPD on EXT2 board which is verified by Arduino M0 Pro.
+ *  This example code is for PDi 2.13" BWR EPD on EXT2 board which is verified by Arduino M0 Pro.
  *  And it sould be able to execute on Arduino IDE supported Board.
  *  Like Arduino Due or Arduino Uno(Need a level shifter 5V -> 3V for EXT2 board)  
  *  For more information about PDi EPD and EXT2 board, please visit 
